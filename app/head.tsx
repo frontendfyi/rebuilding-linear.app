b17f5ca815b6be3b10e7a7c7e9fa12922efbefb9
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-11-26 11:13:02 +0100

    Added a page title and meta description simply because it looks better.

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Rebuilding Linear.app</title>
+      <meta
+        name="description"
+        content="Rebuilding Linear.app's website with Next.js, Tailwind and Framer Motion."
+      />
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
