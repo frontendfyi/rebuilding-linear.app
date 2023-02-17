47a0015dad90b25397b04214417c5462d2548f56
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-02-17 15:13:05 +0100

    Gave feature main title padding on mobile, which ensures the gradient always ends up ABOVE the text.

--- components/features.tsx
+++ components/features.tsx
@@ -58,7 +58,7 @@ const MainFeature = ({
             imageSize === "small" ? "w-[78rem]" : "w-[102.4rem]"
           )}
         >
-          <h2 className="text-gradient mb-11 translate-y-[40%] text-center text-6xl [transition:transform_1000ms_cubic-bezier(0.3,_1.17,_0.55,_0.99)_0s] md:text-8xl [.is-visible_&]:translate-y-0">
+          <h2 className="text-gradient mb-11 translate-y-[40%] pt-[12rem] text-center text-6xl [transition:transform_1000ms_cubic-bezier(0.3,_1.17,_0.55,_0.99)_0s] md:pt-0 md:text-8xl [.is-visible_&]:translate-y-0">
             {title}
           </h2>
           <div className="relative z-10 rounded-[14px] backdrop-blur-[6px] before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(rgba(255,_255,_255,_0.3),_rgba(255,_255,_255,_0)_120%)] before:p-[1px] before:[mask:linear-gradient(black,_black)_content-box_content-box,_linear-gradient(black,_black)] before:[mask-composite:xor] after:pointer-events-none after:absolute after:inset-0 after:rounded-[inherit] after:bg-[rgba(255,_255,_255,_0.15)] after:[mask:linear-gradient(black,transparent)]">
