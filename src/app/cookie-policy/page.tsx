f5378d5d1915c903d7881dd5c203683a9a80fb0b
Zhaomian Zhao <loisisar@outlook.com>
2023-02-27 14:18:21 +0000

    add cookie policy but uncessessful

--- src/app/cookie-policy/page.tsx
+++ src/app/cookie-policy/page.tsx
@@ -1,20 +1,25 @@
 "use client";
 import { Container } from "../../components/container";
 import React, { useEffect } from "react";
+import Script from "next/script";
 
 export default function TermsCondition() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://app.termly.io/embed-policy.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
   return (
-    <div
-      data-name="termly-embed"
-      data-id="5d205abf-0536-48c2-b628-340e35bc2cc2"
-      data-type="iframe"
-    ></div>
+    <>
+      <Script
+        id="5d205abf-0536-48c2-b628-340e35bc2cc2"
+        dangerouslySetInnerHTML={{
+          __html: `!function(d, s, id) {
+  var js, tjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://app.termly.io/document/cookie-policy/5d205abf-0536-48c2-b628-340e35bc2cc2";
+  tjs.parentNode.insertBefore(js, tjs);
+}(document, 'script', 'termly-jssdk')
+      `,
+        }}
+        src="https://app.termly.io/document/cookie-policy/5d205abf-0536-48c2-b628-340e35bc2cc2"
+      />
+    </>
   );
 }
