95a281aac289eeb50b957cccfe70498aad34c32c
Zhaomian Zhao <loisisar@outlook.com>
2023-02-27 22:03:12 +0000

    edited Inter font at layout but not getting better

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import { AnalyticsWrapper } from "../components/analytics";
 import { Container } from "../components/container";
-import Inter from "@next/font/google";
+import { Inter as FontSans } from "@next/font/google";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import "../styles/globals.css";
+import { cn } from "../utils/cn";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createServerClient } from "../utils/supabase-server";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
